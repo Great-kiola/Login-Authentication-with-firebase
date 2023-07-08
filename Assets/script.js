37efa97eb6c64f3e5e5b070fb4cac157452361ca
Ola <hollabodey.kilaso@gmail.com>
2023-07-08 06:14:09 +0100

    Made changes to the index and js file

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,13 @@
 const toggle = document.querySelector('.toggle');
 const input = document.querySelector('input');
 
+var clicked = 0;
+
 toggle.addEventListener('click', () => {
 
+    clicked++;
+    console.log(clicked);
+    
     if (input.type === "password") {
         input.type = "text";
     } else {
