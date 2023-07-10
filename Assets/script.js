c5e01633e0bca6a3d91135bbba50c048524ee99d
Ola <hollabodey.kilaso@gmail.com>
2023-07-10 23:33:55 +0100

    modify: Validate the login for the inputs

--- Assets/script.js
+++ Assets/script.js
@@ -8,10 +8,35 @@ const github = document.getElementById('github');
 // Login button
 const login = document.getElementById('login');
 
-login.addEventListener('click', function (e) {
+// Inputs
+let emailInput = document.getElementById('email');
+let input = document.querySelector('input');
+
+
+// Vlaidate email function
+function validateEmail(){
+    let validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    
+    if (emailInput.value.match(validRegex)) {
+        // email.style.display = "none";
+        input.classList.remove("error")
+        return true;
+    } else {
+        // email.style.display = "flex";
+        input.classList.add("error");
+    }
+}
+
+// Clicking on the Login button
+login.addEventListener("click", function(e){
     e.preventDefault();
-    alert('You have successfully logged in');
-});
+
+    if (validateEmail()){
+        input.classList.remove("error")
+    }
+
+})
 
 // Google Auth
 google.addEventListener('click', function(e) {
